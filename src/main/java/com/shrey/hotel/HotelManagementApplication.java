bbc087298505de3ef6e9bb7dedb3f00aae954ac7
Developer <dev@example.com>
2025-11-29 19:20:31 +0530

    Upgrade project to use `Spring Boot 3.3.x` using openrewrite.

--- src/main/java/com/shrey/hotel/HotelManagementApplication.java
+++ src/main/java/com/shrey/hotel/HotelManagementApplication.java
@@ -14,7 +14,7 @@ public class HotelManagementApplication {
     }
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
+    WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
--- src/main/java/com/shrey/hotel/model/FoodItem.java
+++ src/main/java/com/shrey/hotel/model/FoodItem.java
@@ -1,7 +1,9 @@
 package com.shrey.hotel.model;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 
 @Entity
